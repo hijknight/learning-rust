1160b66ef790221b42ea33e8ff705323b6f90558
H20rj <navybluehowboutyou@gmail.com>
2025-01-10 15:41:48 -0600

    finished 4.3

--- 4-ownership/4.3-slice/src/main.rs
+++ 4-ownership/4.3-slice/src/main.rs
@@ -1,25 +1,64 @@
+// fn main() {
+//     let s = String::from("hello world"); // first word ends at 5, so should return 5
+//
+//     let word: &str = first_word(&s);
+//
+//     println!("{word}");
+// }
+
 fn main() {
-    let mut s = String::from("hello world"); // first word ends at 5, so should return 5
+    let string = String::from("hello world");
 
-    let word: usize = first_word(&s);
+    let word = first_word(&string[6..11]);
+    let word = first_word(&string[..]);
 
-    s.clear();
+    let word = first_word(&string);
+
+    let str_lit = "Hello world";
+
+    let word = first_word(&str_lit[0..5]);
+    let word = first_word(&str_lit[..]);
+
+    let word = first_word(str_lit);
+
 
     println!("{word}");
+
+    let a = [1, 2, 3, 4, 5];
+
+    let slice_a = &a[1..3];
+
+    assert_eq!(slice_a, &[2, 3]);
+
+
+
 }
 
-fn first_word(s: &String) -> usize {
+
+// first word 1
+// fn first_word(s: &String) -> usize {
+//     let bytes = s.as_bytes();
+//
+//     for (i, &item) in bytes.iter().enumerate() {
+//         if item == b' ' {
+//             return i;
+//         }
+//     }
+//
+//     s.len()
+// }
+
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
 
-    s.len()
+    &s[..]
 }
 
-fn second_word() {
 
-}
+
