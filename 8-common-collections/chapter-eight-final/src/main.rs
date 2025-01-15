8cacf9d0394d10d9d1c559e33cb931a753b28f46
H20rj <navybluehowboutyou@gmail.com>
2025-01-15 10:34:27 -0600

    Updated chapter final

--- 8-common-collections/chapter-eight-final/src/main.rs
+++ 8-common-collections/chapter-eight-final/src/main.rs
@@ -2,51 +2,29 @@
 // return the median (when sorted, the value in the middle position)
 // and mode (the value that occurs most often; a hash map will be helpful here) of the list.
 
-use std::collections::HashMap;
+mod lib;
+use lib::{ median, mode };
+
 
 fn main() {
 
     let nums = vec![1, 5, 5, 3, 3, 3, 6, 4, 2, 20, 10];
 
-    println!("median is {}\nmode is {}", median(nums.clone()), mode(nums));
-}
 
-fn median(mut list: Vec<i32>) -> f64 {
-    list.sort();
-
-    if list.len() % 2 == 0 {
-        let mid = list.len() / 2;
-        let mid_1 = &mid - 1;
-
-        (list[mid] as f64 + list[mid_1] as f64) / 2.0
-
-    } else {
-        let mid = list.len() / 2;
-        list[mid] as f64
+    match median(nums.clone()) {
+        Some(m) => println!("Median: {}", m),
+        None => println!("No median found"),
     }
 
+    match mode(&nums) {
+        Some(m) => println!("Mode: {}", m),
+        None => println!("No mode found"),
+    }
 }
 
-fn mode(list: Vec<i32>) -> i32 {
 
-    let mut map = HashMap::new();
 
-    for num in list {
-        let count = map.entry(num).or_insert(0);
-        *count += 1;
-    }
-    let mut max = 0;
-    let mut mode = 0;
 
-    for (key, value) in map {
-        if value > max {
-            max = value;
-            mode = key;
-        }
-    }
-    mode
-
-}
 
 
 
