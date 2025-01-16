70b5da7e8816b18a7f40647711930399062f5b6a
H20rj <navybluehowboutyou@gmail.com>
2025-01-16 17:47:47 -0600

    minor changes

--- 9-error-handling/9.2-result/src/main.rs
+++ 9-error-handling/9.2-result/src/main.rs
@@ -3,33 +3,36 @@ use std::io::{self, Read};
 
 fn main() {
 
-     // let greeting_file = match greeting_result {
-     //     Ok(file) => file,
-     //     Err(error) => match error.kind() {
-     //         ErrorKind::NotFound => match File::create("hello.txt") {
-     //             Ok(fc) => fc,
-     //             Err(e) => panic!("Error occurred. {e:?}"),
-     //         },
-     //         other_error => {
-     //             panic!("Problem opening the file: {other_error:?}")
-     //         }
-     //     }
-     // };
-
-//     let greeting = File::open("hello.txt").unwrap_or_else(|error| {
-//         if error.kind() == ErrorKind::NotFound {
-//             File::create("hello.txt").unwrap_or_else(|error| {
-//                 panic!("file could not be created");
-//             })
-//         } else {
-//             panic!("A problem occurred when opening file: {error:?}")
-//         }
-//     });
+    // let greeting_result = File::open("username.txt");
+    //
+    //
+    //  let greeting_file = match greeting_result {
+    //      Ok(file) => file,
+    //      Err(error) => match error.kind() {
+    //          ErrorKind::NotFound => match File::create("hello.txt") {
+    //              Ok(fc) => fc,
+    //              Err(e) => panic!("Error occurred. {e:?}"),
+    //          },
+    //          other_error => {
+    //              panic!("Problem opening the file: {other_error:?}")
+    //          }
+    //      }
+    //  };
+    //
+    // let greeting = File::open("hello.txt").unwrap_or_else(|error| {
+    //     if error.kind() == ErrorKind::NotFound {
+    //         File::create("hello.txt").unwrap_or_else(|error| {
+    //             panic!("file could not be created");
+    //         })
+    //     } else {
+    //         panic!("A problem occurred when opening file: {error:?}")
+    //     }
+    // });
 
 
    // let greeting_file = File::open("hello.txt").expect("No file found");
 
-    let mut username = read_username_from_file("hello.txt").unwrap();
+    let mut username = read_username_from_file("username.txt").unwrap();
 
 
     println!("{username:?}")
