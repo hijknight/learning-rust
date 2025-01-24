60595b4f4592e9d7fa31cc7ece53c88ba8d46ed6
H20rj <navybluehowboutyou@gmail.com>
2025-01-23 20:05:52 -0600

    finished 13.1

--- 13-functional-features/13.1-closures/ownership/src/main.rs
+++ 13-functional-features/13.1-closures/ownership/src/main.rs
@@ -1,14 +1,27 @@
 use std::thread;
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+
 
 fn main() {
+    let mut rec_lst= [
+        Rectangle { width: 10, height: 3 },
+        Rectangle { width: 13, height: 14 },
+        Rectangle { width: 3, height: 5 },
+    ];
 
-    let list = vec![1, 2, 3];
-    println!("Before defining closure: {list:?}");
+    rec_lst.sort_by_key(|r| r.width);
+    println!("{rec_lst:?}")
 
-    thread::spawn(move || println!("From thread: {list:?}"))
-        .join().unwrap();
-    
-
+    // let list = vec![1, 2, 3];
+    // println!("Before defining closure: {list:?}");
+    //
+    // thread::spawn(move || println!("From thread: {list:?}"))
+    //     .join().unwrap();
 
     // let mut list = vec![1, 2, 3];
     //
