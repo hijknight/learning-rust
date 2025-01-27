5929924a869e6adeba89f86edbfd501e96fae71b
hijknight <navybluehowboutyou@gmail.com>
2025-01-27 13:37:09 -0600

    working on probabilty

--- xx-probability/src/lib.rs
+++ xx-probability/src/lib.rs
@@ -1,75 +1,75 @@
-use std::{ env, process };
 use rand::Rng;
+use std::{env, process};
 
 pub struct Config {
     pub num_flips: i32,
     pub iterations: i32,
-    pub use_perfect: bool,
+    pub test: bool,
 }
 
 pub fn run(config: Config) {
     let num_flips = config.num_flips;
     let iterations = config.iterations;
 
+    if !config.test {
+        let mut rng = rand::thread_rng();
+        let mut total_tries = 0;
 
+        for _ in 0..iterations {
+            let mut counter = 0;
+            let mut perfect = false;
 
-    for _ in 0..iterations {
-        let mut perfect: i32 = 0;
-        let mut counter: i32 = 0;
-        if config.use_perfect {
-            while perfect < 1 {
-
+            while !perfect {
                 counter += 1;
-                // println!("\nRun: {}", counter);
-                let mut tails: i32 = 0;
-                let mut heads: i32 = 0;
+                let mut heads = 0;
+                let mut tails = 0;
 
                 for _ in 0..num_flips {
-                    let flip: i32 = rand::thread_rng().gen_range(1..=2);
-
+                    let flip: i32 = rng.gen_range(1..=2);
                     if flip == 1 {
                         heads += 1;
                     } else {
                         tails += 1;
                     }
                 }
-                // println!("Heads: {}", heads);
-                // println!("Tails: {}", tails);
-                let probability = heads as f64 / (heads as f64 + tails as f64);
-                if probability == 0.5 {
-                    perfect += 1;
+
+                if heads == tails {
+                    perfect = true;
                 }
-                println!("Run {}: {}", counter, probability);
             }
 
-            println!("Got a perfect in {} tries", counter)
-        } else {
-            let mut tails: i32 = 0;
-            let mut heads: i32 = 0;
+            println!("Got a perfect 50/50 in {} tries", counter);
+            total_tries += counter;
+        }
+
+        let avg_tries = total_tries as f64 / iterations as f64;
+        println!("Average tries until perfect is: {}", avg_tries);
+        println!("Tested {} times", iterations);
+    } else {
+        let mut rng = rand::thread_rng();
+        for _ in 0..iterations {
+            let mut heads = 0;
+            let mut tails = 0;
 
             for _ in 0..num_flips {
-                let flip: i32 = rand::thread_rng().gen_range(1..=2);
-
+                let flip: i32 = rng.gen_range(1..=2);
                 if flip == 1 {
                     heads += 1;
                 } else {
                     tails += 1;
                 }
             }
-            // println!("Heads: {}", heads);
-            // println!("Tails: {}", tails);
-            let probability = heads as f64 / (heads as f64 + tails as f64);
-            println!("Probability with {}: {}", num_flips, probability);
+
+            let proportion_heads = heads as f64 / (heads + tails) as f64;
+            println!("Proportion heads: {}", proportion_heads);
         }
-
     }
-
 }
 
 impl Config {
     pub fn build(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 2 {
-            return Err("not enough arguments")
+            return Err("not enough arguments");
         }
 
         let num_flips: i32 = args[1].clone().parse().unwrap_or_else(|err| {
@@ -85,9 +85,12 @@ impl Config {
         } else {
             1
         };
+        let test = env::var("TEST").is_ok();
 
-        let use_perfect = env::var("USE_PERFECT").is_ok();
-
-        Ok(Config { num_flips, iterations, use_perfect })
+        Ok(Config {
+            num_flips,
+            iterations,
+            test,
+        })
     }
 }
